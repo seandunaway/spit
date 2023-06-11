957cc292f1321ee05c6cc8892498c58457366aac
seandunaway <git@seandunaway.com>
2023-06-11 06:06:24 -0700

    catch fetch

--- spoo.mjs
+++ spoo.mjs
@@ -1,27 +1,34 @@
 #!/usr/bin/env node
 
-let base_url = 'https://query1.finance.yahoo.com/v8/finance/chart/'
-let symbol = 'ES=F'
-let interval = '5m'
-let range = '5d'
+export async function yfinance () {
+    let base_url = 'https://query1.finance.yahoo.com/v8/finance/chart/'
+    let symbol = 'ES=F'
+    let interval = '5m'
+    let range = '5d'
+    let url = `${base_url}${symbol}?&interval=${interval}&range=${range}`
 
-let url = `${base_url}${symbol}?&interval=${interval}&range=${range}`
-let response = await fetch (url)
-let json = await response.json ()
-let result = json.chart.result[0]
+    let result
+    try {
+        let response = await fetch (url)
+        let json = await response.json ()
+        result = json.chart.result[0]
+    } catch { return }
 
-let spoo = []
-for (let i = 0; i <= result.timestamp.length; i++) {
-    let date = new Date (result.timestamp[i] * 1000)
+    let spoo = []
+    for (let i = 0; i <= result.timestamp.length; i++) {
+        let date = new Date (result.timestamp[i] * 1000)
 
-    let timestamp = date.getTime ()
-    let price = result.indicators.quote[0].close[i]
+        let timestamp = date.getTime ()
+        let price = result.indicators.quote[0].close[i]
 
-    let quote = {
-        timestamp,
-        price,
+        let quote = {
+            timestamp,
+            price,
+        }
+        spoo .push (quote)
     }
-    spoo .push (quote)
+    return spoo
 }
 
-export default spoo
+export default await yfinance ()
+
