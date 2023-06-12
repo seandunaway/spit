3d5825a3eb9df1ebea3f0357ed2d2c44dcca446e
seandunaway <seandunaway@gmail.com>
2023-06-12 06:45:15 -0700

    smaller points

--- spoo.mjs
+++ spoo.mjs
@@ -3,8 +3,8 @@
 export default async function () {
     let base_url = 'https://query1.finance.yahoo.com/v8/finance/chart/'
     let symbol = 'ES=F'
-    let interval = '5m'
-    let range = '5d'
+    let interval = '2m'
+    let range = '1d'
     let url = `${base_url}${symbol}?&interval=${interval}&range=${range}`
 
     let result
